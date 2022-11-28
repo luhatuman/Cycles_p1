e148f6bde30af8429062c3856804e2c80fee4f28
itsOtto <luhatuman@yandex.ru>
2022-11-28 22:56:17 +0900

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        //Task 1
         System.out.println("Task 1");
         for (int f = 1; f <= 10; f++) {
+            System.out.println(f);
+        }
+        //Task 2
+        System.out.println();
+        System.out.println("Task 2");
+        for (int f = 10; f >= 1; f--) {
             System.out.println(f);
         }
     }
