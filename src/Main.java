161a8b72e0e161c867aba1d6d94ad0960ac90173
itsOtto <luhatuman@yandex.ru>
2022-11-28 22:53:29 +0900

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task 1");
+        for (int f = 1; f <= 10; f++) {
+            System.out.println(f);
+        }
     }
 }
